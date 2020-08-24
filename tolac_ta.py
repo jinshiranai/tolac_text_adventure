aba56db41e9c8812b278c03ef65d9a0679c89992
jinshiranai <jin.shiranai@gmail.com>
2020-08-24 20:33:26 +0900

    Initialize tolac text adventure project

---
+++ tolac_ta.py
@@ -0,0 +1,47 @@
+#############################################################################
+##                                                                          #
+## Tales of Lux and Cor: Text Adventure                                     #
+##                                                                          #
+## Programmed by: Jin Fujikake                                              #
+## Project Start: 2020-Aug-23                                               #
+## Program Description: Text adventure game based on characters from a      #
+##  short story series written by the programmer.                           #
+##                                                                          #
+#############################################################################
+
+#############################################################################
+#---------------------------------------------------------------------------#
+#STORY                                                                      #
+#   You are a kobold, and you are very, very cross with your king.          #
+#                                                                           #
+#   The neighboring kingdoms of Lux and Cor have always had an...           #
+# interesting relationship. Once a generation, the King of Lux and the King #
+# of Cor meet to discuss the futures of their kingdoms. And then battle to  #
+# the death. The winner of the post-summit duel to the death has their      #
+# desires as expressed in the meeting met, and the survivors of the fallen  #
+# king must meet them to the best of their kindgom's ability.               #
+#                                                                           #
+#   During the last cycle, the Dragons, your progenitors and the very       #
+# reason you exist, expressed a desire to shed their Coran citizenship for  #
+# Luxan. All well and good, you thought. You had no particular problems     #
+# with Cor, it was your home, after all. But as long as the Dragons were    #
+# happy, so were you. But after the destined battle, everything changed.    #
+# Suddenly, kobolds were no longer treated as the noble servants of dragons #
+# that they were born and bred to be. Suddenly, kobolds were a stain on     #
+# the Coran name. You turned to your Dragon for explanation, only to find   #
+# that this was the desire of the new King of Cor. The Dragons were welcome #
+# to move to Lux, but they were to be seperated from their own creation,    #
+# the kobolds. And the kobolds were to be treated as though they were no    #
+# better than petty theives in the night.                                   #
+#                                                                           #
+#   This is what your king, the new King of Cor, decreed in the wake of the #
+# Luxan/Coran summit. Your king not only failed you, but separated you from #
+# your reason to exist. It was no surprise that you and many of your kind   #
+# ended up becoming the very things you were made out to be in the public   #
+# eye without your Dragons. But no matter how bad things got or how much    #
+# things changed, you were certain of one thing:                            #
+#                                                                           #
+# If you ever saw the King of Cor, you would take your revenge.             #
+#---------------------------------------------------------------------------#
+#############################################################################
+
